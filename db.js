725b6b9c309a5f3d5568d854683479cedfa3e9d3
VinRis <71922931+VinRis@users.noreply.github.com>
2025-12-24 09:25:51 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,59 +1,38 @@
-const DB_NAME = 'DairyFarmDB';
-const DB_VERSION = 1;
-
 const db = {
-    _db: null,
+    dbName: 'DairyFarmDB',
+    version: 1,
 
-    async init() {
+    init() {
         return new Promise((resolve, reject) => {
-            const request = indexedDB.open(DB_NAME, DB_VERSION);
-
+            const request = indexedDB.open(this.dbName, this.version);
             request.onupgradeneeded = (e) => {
-                const d = e.target.result;
-                if (!d.objectStoreNames.contains('settings')) d.createObjectStore('settings', { keyPath: 'id' });
-                if (!d.objectStoreNames.contains('production')) d.createObjectStore('production', { keyPath: 'id', autoIncrement: true });
-                if (!d.objectStoreNames.contains('finance')) d.createObjectStore('finance', { keyPath: 'id', autoIncrement: true });
-                if (!d.objectStoreNames.contains('health')) d.createObjectStore('health', { keyPath: 'id', autoIncrement: true });
-                if (!d.objectStoreNames.contains('inventory')) d.createObjectStore('inventory', { keyPath: 'id', autoIncrement: true });
+                const db = e.target.result;
+                db.createObjectStore('production', { keyPath: 'id', autoIncrement: true });
+                db.createObjectStore('finance', { keyPath: 'id', autoIncrement: true });
+                db.createObjectStore('health', { keyPath: 'id', autoIncrement: true });
+                db.createObjectStore('settings', { keyPath: 'key' });
             };
-
-            request.onsuccess = (e) => {
-                this._db = e.target.result;
-                resolve();
-            };
-            request.onerror = (e) => reject(e);
-        });
-    },
-
-    async save(storeName, data) {
-        const tx = this._db.transaction(storeName, 'readwrite');
-        const store = tx.objectStore(storeName);
-        return new Promise((resolve) => {
-            const req = store.put(data);
-            req.onsuccess = () => resolve(req.result);
+            request.onsuccess = (e) => resolve(e.target.result);
+            request.onerror = (e) => reject(e.target.error);
         });
     },
 
     async getAll(storeName) {
-        const tx = this._db.transaction(storeName, 'readonly');
-        const store = tx.objectStore(storeName);
+        const database = await this.init();
         return new Promise((resolve) => {
-            const req = store.getAll();
-            req.onsuccess = () => resolve(req.result);
+            const tx = database.transaction(storeName, 'readonly');
+            const store = tx.objectStore(storeName);
+            const request = store.getAll();
+            request.onsuccess = () => resolve(request.result);
         });
     },
 
-    async delete(storeName, id) {
-        const tx = this._db.transaction(storeName, 'readwrite');
-        const store = tx.objectStore(storeName);
-        return new Promise((resolve) => {
-            const req = store.delete(id);
-            req.onsuccess = () => resolve();
-        });
-    },
-
-    async clearStore(storeName) {
-        const tx = this._db.transaction(storeName, 'readwrite');
-        tx.objectStore(storeName).clear();
+    async exportAllData() {
+        const stores = ['production', 'finance', 'health', 'settings'];
+        const exportData = {};
+        for (const store of stores) {
+            exportData[store] = await this.getAll(store);
+        }
+        return exportData;
     }
 };
